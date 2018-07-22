b9c326edd5235c4cb5af601a53da28e2e03a705b
David-xin <xin490884711@hotmail.com>
2018-07-22 11:48:10 +0800

    python list

---
+++ pytho_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#请用索引取出下面list的指定元素：
+
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+
+# 打印Apple:
+print('%s' % L[0][0])
+# 打印Python:
+print('%s' % L[1][1])
+# 打印Lisa:
+print('%s' % L[2][2])
