060bfbd5c3923a6049448ed7f9f5223af462da1a
David-xin <xin490884711@hotmail.com>
2018-07-23 22:37:21 +0800

    add and if python

--- if.py
+++ if.py
@@ -16,9 +16,9 @@ print ('小明的BMI值是%d' % BMI)
 
 if BMI < 18.5:
     print('过轻')
-elif BMI <25:
+elif BMI >=18.5 and BMI <25:
     print ('正常')
-elif BMI < 32:
+elif BMI >= 25 and BMI < 32:
     print ('肥胖')
 else:
     print ('严重肥胖')
